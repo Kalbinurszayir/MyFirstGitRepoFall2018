8b6d6b98a5e4991bdb2587d421f5cb414dfb2f69
KalinburZayir <algakocha@gmail.com>
2019-01-28 23:09:48 -0500

    Hi Asiya

--- src/GitHub.java
+++ src/GitHub.java
@@ -1,6 +1,9 @@
 public class GitHub {
     public static void main(String[] args) {
         System.out.println(" Hello word");
+        System.out.println("Hi Asiya");
+        System.out.println("Java");"git"
+
     }
 
 
